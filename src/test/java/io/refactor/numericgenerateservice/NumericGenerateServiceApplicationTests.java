04735bc42134c177602003e54c53f82beaa4f20e
DTPlayer <dtplayer1@ya.ru>
2025-02-10 19:28:51 +0300

    add test

--- src/test/java/io/refactor/numericgenerateservice/NumericGenerateServiceApplicationTests.java
+++ src/test/java/io/refactor/numericgenerateservice/NumericGenerateServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package io.refactor.numericgenerateservice;
 
+import io.refactor.numericgenerateservice.utils.CodeGenerator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ActiveProfiles("local")
 @SpringBootTest
 class NumericGenerateServiceApplicationTests {
 
+    @Autowired
+    private CodeGenerator codeGenerator;
+
     @Test
-    void contextLoads() {
+    void generateNumber() {
+        Optional<Number> code = codeGenerator.generateNumber();
+
+        assertTrue(code.isPresent());
     }
 
 }
